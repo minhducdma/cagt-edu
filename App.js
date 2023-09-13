49a65fb27ad15492e112fa5a204e514ef93df63b
ducphamM2VN <duc.pm@mmh.co.jp>
2023-09-13 20:55:02 +0700

    chore: creating project

--- App.js
+++ App.js
@@ -4,17 +4,21 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+const colors = {
+  white: '#fff',
+};
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: colors.white,
+    flex: 1,
     justifyContent: 'center',
   },
 });
