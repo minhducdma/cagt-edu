af86b566804976aa211a9386963a731991cb1f1c
ducphamM2VN <duc.pm@mmh.co.jp>
2023-09-15 02:42:57 +0700

    chore: convert js to tsx file

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React, { ReactNode } from 'react';
+import LoginScreen from './screens/LoginScreen';
+
+const App: () => ReactNode = () => {
+  return <LoginScreen />;
+};
+
+export default App;
