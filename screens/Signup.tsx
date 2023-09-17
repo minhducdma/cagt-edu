8ae5ccacabde7a2472ee269941ae36c324a50334
ducphamM2VN <duc.pm@mmh.co.jp>
2023-09-17 21:49:56 +0700

    feat: adding login page, sign up page and the welcome page

--- screens/Signup.tsx
+++ screens/Signup.tsx
@@ -1,19 +1,313 @@
-import { StyleSheet, Text, View } from 'react-native';
+import COLORS from '@/constants/theme-constants';
+import { Ionicons } from '@expo/vector-icons';
+import Checkbox from 'expo-checkbox';
+import React, { useState } from 'react';
+import Button from '@/components/ui/Button';
+import {
+  SafeAreaView,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Pressable,
+  Text,
+  Image,
+} from 'react-native';
 
-const Signup = () => {
+const Signup = ({ navigation }) => {
+  const [isPasswordShown, setIsPasswordShown] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>SignUp Screen</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
+      <View style={{ flex: 1, marginHorizontal: 22 }}>
+        <View style={{ marginVertical: 22 }}>
+          <Text
+            style={{
+              fontSize: 22,
+              fontWeight: 'bold',
+              marginVertical: 12,
+              color: COLORS.black,
+            }}
+          >
+            Tạo tài khoản
+          </Text>
+
+          <Text
+            style={{
+              fontSize: 16,
+              color: COLORS.black,
+            }}
+          >
+            Kết nối với chúng tôi!
+          </Text>
+        </View>
+
+        <View style={{ marginBottom: 12 }}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: '400',
+              marginVertical: 8,
+            }}
+          >
+            Địa chỉ email
+          </Text>
+
+          <View
+            style={{
+              width: '100%',
+              height: 48,
+              borderColor: COLORS.black,
+              borderWidth: 1,
+              borderRadius: 8,
+              alignItems: 'center',
+              justifyContent: 'center',
+              paddingLeft: 22,
+            }}
+          >
+            <TextInput
+              placeholder="Nhập địa chỉ email"
+              placeholderTextColor={COLORS.black}
+              keyboardType="email-address"
+              style={{
+                width: '100%',
+              }}
+            />
+          </View>
+        </View>
+
+        <View style={{ marginBottom: 12 }}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: '400',
+              marginVertical: 8,
+            }}
+          >
+            Số điện thoại
+          </Text>
+
+          <View
+            style={{
+              width: '100%',
+              height: 48,
+              borderColor: COLORS.black,
+              borderWidth: 1,
+              borderRadius: 8,
+              alignItems: 'center',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingLeft: 22,
+            }}
+          >
+            <TextInput
+              placeholder="+91"
+              placeholderTextColor={COLORS.black}
+              keyboardType="numeric"
+              style={{
+                width: '12%',
+                borderRightWidth: 1,
+                borderLeftColor: COLORS.grey,
+                height: '100%',
+              }}
+            />
+
+            <TextInput
+              placeholder="Nhập số điện thoại"
+              placeholderTextColor={COLORS.black}
+              keyboardType="numeric"
+              style={{
+                width: '80%',
+              }}
+            />
+          </View>
+        </View>
+
+        <View style={{ marginBottom: 12 }}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: '400',
+              marginVertical: 8,
+            }}
+          >
+            Password
+          </Text>
+
+          <View
+            style={{
+              width: '100%',
+              height: 48,
+              borderColor: COLORS.black,
+              borderWidth: 1,
+              borderRadius: 8,
+              alignItems: 'center',
+              justifyContent: 'center',
+              paddingLeft: 22,
+            }}
+          >
+            <TextInput
+              placeholder="Nhập mật khẩu"
+              placeholderTextColor={COLORS.black}
+              secureTextEntry={isPasswordShown}
+              style={{
+                width: '100%',
+              }}
+            />
+
+            <TouchableOpacity
+              onPress={() => setIsPasswordShown(!isPasswordShown)}
+              style={{
+                position: 'absolute',
+                right: 12,
+              }}
+            >
+              {isPasswordShown === true ? (
+                <Ionicons name="eye-off" size={24} color={COLORS.black} />
+              ) : (
+                <Ionicons name="eye" size={24} color={COLORS.black} />
+              )}
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            marginVertical: 6,
+          }}
+        >
+          <Checkbox
+            style={{ marginRight: 8 }}
+            value={isChecked}
+            onValueChange={setIsChecked}
+            color={isChecked ? COLORS.primary : undefined}
+          />
+
+          <Text>Tôi đồng ý với tất cả điều khoản</Text>
+        </View>
+
+        <Button
+          title="Đăng ký"
+          filled
+          style={{
+            marginTop: 18,
+            marginBottom: 4,
+          }}
+        />
+
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginVertical: 20,
+          }}
+        >
+          <View
+            style={{
+              flex: 1,
+              height: 1,
+              backgroundColor: COLORS.grey,
+              marginHorizontal: 10,
+            }}
+          />
+          <Text style={{ fontSize: 14 }}>Hoặc đăng nhập bằng</Text>
+          <View
+            style={{
+              flex: 1,
+              height: 1,
+              backgroundColor: COLORS.grey,
+              marginHorizontal: 10,
+            }}
+          />
+        </View>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => console.log('Pressed')}
+            style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+              flexDirection: 'row',
+              height: 52,
+              borderWidth: 1,
+              borderColor: COLORS.grey,
+              marginRight: 4,
+              borderRadius: 10,
+            }}
+          >
+            <Image
+              source={require('@/assets/images/facebook.png')}
+              style={{
+                height: 36,
+                width: 36,
+                marginRight: 8,
+              }}
+              resizeMode="contain"
+            />
+
+            <Text>Facebook</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => console.log('Pressed')}
+            style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+              flexDirection: 'row',
+              height: 52,
+              borderWidth: 1,
+              borderColor: COLORS.grey,
+              marginRight: 4,
+              borderRadius: 10,
+            }}
+          >
+            <Image
+              source={require('@/assets/images/google.png')}
+              style={{
+                height: 36,
+                width: 36,
+                marginRight: 8,
+              }}
+              resizeMode="contain"
+            />
+
+            <Text>Google</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginVertical: 22,
+          }}
+        >
+          <Text style={{ fontSize: 16, color: COLORS.black }}>
+            Bạn đã có tài khoản ?
+          </Text>
+          <Pressable onPress={() => navigation.navigate('Login')}>
+            <Text
+              style={{
+                fontSize: 16,
+                color: COLORS.primary,
+                fontWeight: 'bold',
+                marginLeft: 6,
+              }}
+            >
+              Đăng nhập
+            </Text>
+          </Pressable>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flex: 1,
-    justifyContent: 'center',
-  },
-});
 
 export default Signup;
--- screens/Welcome.tsx
+++ screens/Welcome.tsx
@@ -1,19 +1,165 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Image, Text, Pressable } from 'react-native';
+import Button from '@/components/ui/Button';
+import COLORS from '@/constants/theme-constants';
 
-const Welcome = () => {
+const Welcome = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text>Welcome Screen</Text>
+    <View style={{ flex: 1, backgroundColor: COLORS.primary }}>
+      <View style={{ flex: 1 }}>
+        <Image
+          source={require('@/assets/images/hero1.jpg')}
+          style={{
+            height: 100,
+            width: 100,
+            borderRadius: 20,
+            position: 'absolute',
+            top: 10,
+            transform: [
+              { translateX: 20 },
+              { translateY: 50 },
+              { rotate: '-15deg' },
+            ],
+          }}
+        />
+        <Image
+          source={require('@/assets/images/hero3.jpg')}
+          style={{
+            height: 100,
+            width: 100,
+            borderRadius: 20,
+            position: 'absolute',
+            top: -30,
+            left: 100,
+            transform: [
+              { translateX: 50 },
+              { translateY: 50 },
+              { rotate: '-5deg' },
+            ],
+          }}
+        />
+
+        <Image
+          source={require('@/assets/images/hero3.jpg')}
+          style={{
+            width: 100,
+            height: 100,
+            borderRadius: 20,
+            position: 'absolute',
+            top: 130,
+            left: -50,
+            transform: [
+              { translateX: 50 },
+              { translateY: 50 },
+              { rotate: '15deg' },
+            ],
+          }}
+        />
+
+        <Image
+          source={require('@/assets/images/hero2.jpg')}
+          style={{
+            height: 200,
+            width: 200,
+            borderRadius: 20,
+            position: 'absolute',
+            top: 110,
+            left: 100,
+            transform: [
+              { translateX: 50 },
+              { translateY: 50 },
+              { rotate: '-15deg' },
+            ],
+          }}
+        />
+      </View>
+
+      <View
+        style={{
+          paddingHorizontal: 22,
+          position: 'absolute',
+          top: 400,
+          width: '100%',
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 40,
+            fontWeight: 'bold',
+            color: COLORS.white,
+          }}
+        >
+          Chào mừng đến với
+        </Text>
+        <Text
+          style={{
+            fontSize: 46,
+            fontWeight: 'bold',
+            color: COLORS.white,
+          }}
+        >
+          CAGT APP
+        </Text>
+
+        <View style={{ marginVertical: 22 }}>
+          <Text
+            style={{
+              fontSize: 16,
+              color: COLORS.white,
+              marginVertical: 4,
+            }}
+          >
+            Phần mềm hỗ trợ học sinh, sinh viên hàng đầu
+          </Text>
+          <Text
+            style={{
+              fontSize: 16,
+              color: COLORS.white,
+            }}
+          >
+            Việt Nam
+          </Text>
+        </View>
+
+        <Button
+          title="Tham gia ngay"
+          onPress={() => navigation.navigate('Signup')}
+          style={{
+            marginTop: 22,
+            fontweight: 'bold',
+          }}
+        />
+
+        <View
+          style={{
+            flexDirection: 'row',
+            marginTop: 12,
+            justifyContent: 'center',
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 16,
+              color: COLORS.white,
+            }}
+          >
+            Bạn đã có tài khoản ?
+          </Text>
+          <Pressable onPress={() => navigation.navigate('Login')}>
+            <Text
+              style={{
+                fontSize: 16,
+                color: COLORS.white,
+                fontWeight: 'bold',
+                marginLeft: 4,
+              }}
+            >
+              Đăng nhập
+            </Text>
+          </Pressable>
+        </View>
+      </View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flex: 1,
-    justifyContent: 'center',
-  },
-});
 
 export default Welcome;
